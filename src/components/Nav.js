97adefc28f10ce7929e0e674f1a7ff64f3ade248
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-05 21:04:51 +0530

    bd-colours & orientation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../assets/scripts/nav-header.css";
 
 function NavHeader() {
 	return (
 		<div>
-			<nav></nav>
+			<nav className="nav">
+				<a href="/">Home</a>
+				<a href="/">About</a>
+				<a href="/">Projects</a>
+				<a href="/">Resume</a>
+				<a href="/">Contact</a>
+			</nav>
 
 			<div className="header-body">
 				<h1 className="name-heading">Aryan Sharma</h1>
