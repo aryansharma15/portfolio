f552e1dcd919407a3d5ffbd16f40c16e8d199448
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-05 22:05:16 +0530

    landing head complete until next

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,28 @@ import "../assets/scripts/nav-header.css";
 function NavHeader() {
 	return (
 		<div>
-			<nav className="nav">
-				<a href="/">Home</a>
-				<a href="/">About</a>
-				<a href="/">Projects</a>
-				<a href="/">Resume</a>
-				<a href="/">Contact</a>
-			</nav>
-
 			<div className="header-body">
-				<h1 className="name-heading">Aryan Sharma</h1>
-				<p className="intro-para"></p>
-				<img src="" alt="" className="back-img" />
-				<button className="contact-btn">Contact Me</button>
+				<nav className="nav">
+					<a href="/">Home</a>
+					<a href="/">About</a>
+					<a href="/">Projects</a>
+					<a href="/">Resume</a>
+					<a href="/">Contact</a>
+				</nav>
+
+				<img
+					src="https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+					alt="laptop and monitor"
+					className="back-img"
+					width={600}
+				/>
+				<div className="info">
+					<h1 className="name-heading">Welcome!</h1>
+					<p className="intro-para">
+						I'm Aryan Sharma, a 22 year old with a passion for design, development, football, physics, comics and quizzing.
+					</p>
+					<button className="contact-btn">Get in Touch</button>
+				</div>
 			</div>
 		</div>
 	);
