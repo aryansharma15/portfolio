c715b7b57eda5f109a1ce8eb1bb09f17cc097f56
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-05 20:07:30 +0530

    beginners-p

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavHeader() {
+	return (
+		<div>
+			<nav></nav>
+
+			<div className="header-body">
+				<h1 className="name-heading">Aryan Sharma</h1>
+				<p className="intro-para"></p>
+				<img src="" alt="" className="back-img" />
+				<button className="contact-btn">Contact Me</button>
+			</div>
+		</div>
+	);
+}
+
+export default NavHeader;
