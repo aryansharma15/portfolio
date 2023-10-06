203573fd916112eca8f95d4bc4e87d22f71e3ee5
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-06 21:40:02 +0530

    skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavHeader from "./components/Nav";
+import SkillProject from "./components/SkillProject";
 
 function App() {
 	return (
 		<div>
 			<div className="App"></div>
 			<NavHeader></NavHeader>
+			<SkillProject></SkillProject>
 		</div>
 	);
 }
---
+++ src/components/SkillProject.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function SkillProject() {
+	return (
+		<div>
+			<div className="skills-div">
+				<ul className="lang-tech-skills">
+					<li>React</li>
+					<li>Node.js</li>
+					<li>Express</li>
+					<li>Javascript</li>
+					<li>Tailwind CSS</li>
+					<li>HTML5</li>
+					<li>CSS3</li>
+					<li></li>
+				</ul>
+				<div className="platform-skills"></div>
+			</div>
+
+			<div className="projects-div"></div>
+		</div>
+	);
+}
+
+export default SkillProject;
