ce0efc873785a1e7f52c24c48ba2b45f1a110c0a
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-06 22:07:18 +0530

    sk-css

--- src/components/SkillProject.js
+++ src/components/SkillProject.js
@@ -1,13 +1,18 @@
 import React from "react";
+import "../assets/scripts/skill-proj.css";
 
 function SkillProject() {
 	return (
 		<div>
 			<div className="skills-div">
+				<h4>Technological Skills</h4>
+
 				<ul className="lang-tech-skills">
 					<li>React</li>
 					<li>Node.js</li>
 					<li>Express</li>
+					<li>Next.js</li>
+					<li>MongoDB Atlas</li>
 					<li>Javascript</li>
 					<li>Tailwind CSS</li>
 					<li>HTML5</li>
