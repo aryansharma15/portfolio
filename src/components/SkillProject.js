782384de8f39c99d368c62c808029aad6a902af8
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-07 22:52:41 +0530

    skill-plat css left

--- src/components/SkillProject.js
+++ src/components/SkillProject.js
@@ -4,7 +4,7 @@ import "../assets/scripts/skill-proj.css";
 function SkillProject() {
 	return (
 		<div>
-			<div className="wrapper">
+			<div className="wrapper1">
 				<div>
 					<h4>Technological Skills</h4>
 					<p>Skills I use to create websites</p>
@@ -24,17 +24,42 @@ function SkillProject() {
 						<li>CSS3</li>
 						<li></li>
 					</ul>
-					<div className="platform-skills">
-						<ul className="plat-tech-skills">
-							<li>Github</li>
-							<li>Figma</li>
-							<li>Postman</li>
-						</ul>
-					</div>
+				</div>
+			</div>
+			<div className="wrapper2">
+				<div>
+					<h4>Platforms & Softwares</h4>
+					<p></p>
+				</div>
+				<div className="platform-div">
+					<ul className="plat-tech-skills">
+						<li>Github</li>
+						<li>Figma</li>
+						<li>Postman</li>
+						<li>a</li>
+						<li>b</li>
+						<li>c</li>
+					</ul>
 				</div>
 			</div>
 
-			<div className="projects-div"></div>
+			<div className="projects-div">
+				<div>
+					<h2>Proj 1</h2>
+					<h3>Tech Used</h3>
+					<p>Description</p>
+				</div>
+				<div>
+					<h2>Proj 2</h2>
+					<h3>Tech Used</h3>
+					<p>Description</p>
+				</div>
+				<div>
+					<h2>Proj 3</h2>
+					<h3>Tech Used</h3>
+					<p>Description</p>
+				</div>
+			</div>
 		</div>
 	);
 }
