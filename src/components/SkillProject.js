ab35f16c6bd2897238d381ff685c55c8ddbc7083
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-07 14:36:53 +0530

    plats

--- src/components/SkillProject.js
+++ src/components/SkillProject.js
@@ -4,22 +4,34 @@ import "../assets/scripts/skill-proj.css";
 function SkillProject() {
 	return (
 		<div>
-			<div className="skills-div">
-				<h4>Technological Skills</h4>
-
-				<ul className="lang-tech-skills">
-					<li>React</li>
-					<li>Node.js</li>
-					<li>Express</li>
-					<li>Next.js</li>
-					<li>MongoDB Atlas</li>
-					<li>Javascript</li>
-					<li>Tailwind CSS</li>
-					<li>HTML5</li>
-					<li>CSS3</li>
-					<li></li>
-				</ul>
-				<div className="platform-skills"></div>
+			<div className="wrapper">
+				<div>
+					<h4>Technological Skills</h4>
+					<p>Skills I use to create websites</p>
+				</div>
+				<div className="skills-div">
+					<ul className="lang-tech-skills">
+						<a href="https://react.dev/">
+							<li>React</li>
+						</a>
+						<li>Node.js</li>
+						<li>Express</li>
+						<li>Next.js</li>
+						<li>MongoDB Atlas</li>
+						<li>Javascript</li>
+						<li>Tailwind CSS</li>
+						<li>HTML5</li>
+						<li>CSS3</li>
+						<li></li>
+					</ul>
+					<div className="platform-skills">
+						<ul className="plat-tech-skills">
+							<li>Github</li>
+							<li>Figma</li>
+							<li>Postman</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 
 			<div className="projects-div"></div>
